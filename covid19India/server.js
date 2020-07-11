d50c5739f26be9ffd1168937b0d2ff46b4122132
cks <cksinha4878@gmail.com>
2020-07-11 15:53:30 +0530

    for deploying application on heroku

--- covid19India/server.js
+++ covid19India/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/covid19-india'));
+app.use(express.static(__dirname + '/dist/covid19India'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/covid19-india/index.html'));
+res.sendFile(path.join(__dirname+'/dist/covid19India/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
